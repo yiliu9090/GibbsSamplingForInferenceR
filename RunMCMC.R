304aa4909d59c42eb8070119542aa7c810488586
YI LIU <Yi.liu9090@gmail.com>
2022-01-05 01:03:55 -0600

    debug

--- RunMCMC.R
+++ RunMCMC.R
@@ -418,13 +418,13 @@ for(k in 1:length(config$DATA_LOCATION)){
   arrows(x0=jittN, y0=var.est.l-nsd*var.est.s, x1=jittN, y1=var.est.l+nsd*var.est.s, code=3, angle=90, length=0.1)
   dev.off()
 
-  dis.a = min(abs(diff(log10(AlphaCollection))))
+  dis.a = min(abs(diff(log10(var.est.a))))
 
   SettingsChar = paste0("GAMMAAB",as.character(gamma.prior.a),"_",as.character(gamma.prior.b),"MCMC",as.character(iter),"BURN",as.character(burnin),"LVSA")
   LVNplot_name = paste0(config$DUMP_LOCATION[[k]],config$NAME[[k]],SettingsChar,'.pdf' )
   pdf(LVNplot_name)
   LVN_name = "Lambda Estimates Against A"
-  jittA = jitter(var.est.a, 0.01*dis.a)
+  jittA = jitter(log10(var.est.a), 0.01*dis.a)
   plot(var.est.l ~ jittA, pch = 15, main = LVN_name, xlab = "log(alpha)", ylab = "Lambdas", sub = SettingsChar)
   arrows(x0=jittA, y0=var.est.l-nsd*var.est.s, x1=jittA, y1=var.est.l+nsd*var.est.s, code=3, angle=90, length=dis.a/2)
   dev.off()
